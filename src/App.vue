7c068e9e4f7c348eb06ea21f5b3b417c9fd155c1
HarounaTraore <167339173+HarounaTraore@users.noreply.github.com>
2024-08-06 10:42:57 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+<HelloWorld></HelloWorld>
+</template>
+  <script >
+  import  HelloWorld  from "./components/helloWorld.vue";
+
+  export default {
+    name: "App",
+    components: {
+      HelloWorld
+    }
+  }
+  </script>
+
+<style scoped></style>
---
+++ src/components/helloWorld.vue
@@ -0,0 +1,21 @@
+vue.component(HelloWorld {
+    <template>
+        <h1><span>Hello</span> world !</h1>
+  </template>
+})
+
+<script>
+ export default{
+ 
+ }
+</script>
+
+<style scoped>
+h1 {
+    font-size: 80px;
+    color: blue;
+}
+span{
+    color: red;
+}
+</style>
